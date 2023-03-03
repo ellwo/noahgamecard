04302c31edf53590d096a417f213413769a0bf26
ellwo <ellw0pc@gmail.com>
2023-03-03 17:59:51 +0300

    cancel crf token

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -34,7 +34,7 @@ return [
             // middleware, this delegates auth and permission checks to the field level.
             \Nuwave\Lighthouse\Support\Http\Middleware\AttemptAuthentication::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+           // \App\Http\Middleware\VerifyCsrfToken::class,
 
             // Logs every incoming GraphQL query.
             // \Nuwave\Lighthouse\Support\Http\Middleware\LogGraphQLQueries::class,
