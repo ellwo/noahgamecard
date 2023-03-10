b490029194534254cf78def8ed0d1a66d6bd6359
ellwo <ellw0pc@gmail.com>
2023-03-10 03:04:26 +0300

    with passport

--- app/GraphQL/Mutations/VerifyPhone.php
+++ app/GraphQL/Mutations/VerifyPhone.php
@@ -22,8 +22,7 @@ final class VerifyPhone
         $this->args=$args;
         // TODO implement the resolver
         $user=User::find(auth()->user()->id);
-         $res=PhoneCode::where('code','=',$args['code'])->where('phone','=',$user->phone)
-         ->where('ex_at','>',now())->first();
+         $res=PhoneCode::where('code','=',$args['code'])->where('phone','=',$user->phone)->first();
 
 
          if($res!=null){
