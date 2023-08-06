7d0242b6071432005ee903bb622965b4750b12d3
ellwo <ellw0pc@gmail.com>
2023-08-06 03:47:23 +0300

    with Old Way

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -72,6 +72,43 @@ class UsersController extends Controller
                 $userNotification=UserNotification::orderBy('id','desc')->first();
 
 
+        $url = 'https://fcm.googleapis.com/fcm/send';
+
+        $dataArr = array('click_action' => 'FLUTTER_NOTIFICATION_CLICK',
+         'id' => $userNotification->id,
+         "data"=>$userNotification->data,
+         'status'=>"done",);
+        $notification = array(
+        'title' =>$userNotification->title,
+        'body' => $userNotification->body,
+        'image'=> $userNotification->img??'',
+        'sound' => 'default',
+        'badge' => '1',);
+        $arrayToSend = array(
+        'registration_ids' => $userNotification->user->f_token->pluck('token')->toArray(),
+        'notification' => $notification,
+        'data' => $dataArr,
+        'priority'=>'high');
+
+        $fields = json_encode ($arrayToSend);
+        $headers = array (
+            'Authorization: key=' . config('firebase.server_key'),
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init ();
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POST, true );
+        curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
+
+        $result =
+         curl_exec ( $ch );
+
+        return var_dump($result);
+        curl_close ( $ch );
+
          $notificationData = [
             'title' => $userNotification->title,
             'body' => $userNotification->body,
