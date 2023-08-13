655895fc6e9eaaf2b8cc18022198a01065f66308
ellwo <95657031+ellwo@users.noreply.github.com>
2023-08-13 22:30:47 +0300

    with fix email

--- app/Notifications/CustomPasswordCodeNotification.php
+++ app/Notifications/CustomPasswordCodeNotification.php
@@ -49,7 +49,7 @@ class CustomPasswordCodeNotification extends Notification
         ->subject(Lang::get('Reset Password Notification'))
         ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
         ->action($this->code, "#")
-        ->line(Lang::get('This password code will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+        ->line(Lang::get('سوف ينتهي صلاحية هذا الكود بعد  in :count دقيقة.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
         ->line(Lang::get('If you did not request a password reset, no further action is required.'));
 }
 
