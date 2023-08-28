1539a817e89e841cbe52cb2cb16f30d893f56bc1
ellwo <95657031+ellwo@users.noreply.github.com>
2023-08-29 00:58:54 +0300

    with Last Report

--- app/GraphQL/Queries/Qgetemail.php
+++ app/GraphQL/Queries/Qgetemail.php
@@ -39,7 +39,7 @@ final class Qgetemail
 
 
                 if(
-                    Auth::guard('web')->attempt(["email"=>$user->email,"password"=>$args["password"]],false))
+                    Auth::attempt(["email"=>$user->email,"password"=>$args["password"]],false))
                 {
 
                     if($user->isBanned())
