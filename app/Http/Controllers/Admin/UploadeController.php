8b9b35eff704e7a6366624c2b76ec54bd03040aa
mrzoom <mrzoom@zoom.zoom.com>
2023-12-28 21:55:49 +0300

    With Uploade Prove

--- app/Http/Controllers/Admin/UploadeController.php
+++ app/Http/Controllers/Admin/UploadeController.php
@@ -27,7 +27,7 @@ class UploadeController extends Controller
 
             $foldername='images';
             $folder = uniqid();
-            $ext = strtolower(pathinfo($_FILES['file'],PATHINFO_EXTENSION));
+            $ext = strtolower(pathinfo($_FILES['file'][0],PATHINFO_EXTENSION));
             $name=$folder.now()->timestamp.$ext;
             $path=$foldername.'/'.$name;
 
