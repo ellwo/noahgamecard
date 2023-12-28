9c61427778368573106a481cefb48eb0e1a11047
mrzoom <mrzoom@zoom.zoom.com>
2023-12-28 22:53:01 +0300

    With Uploade Prove

--- resources/views/admin/rassed/table.blade.php
+++ resources/views/admin/rassed/table.blade.php
@@ -260,13 +260,13 @@
                                 <hr>
                                 <div class="md:hidden justify-center">
                                 <span class="font-bold text-blue-900 dark:text-gray-200">{{ $paymentinfo->paymentmethod->name }}</span>
+
+                                </div>
                                 <hr>
                                 <div class=" justify-center">
                                 <span class="font-bold text-blue-900 dark:text-gray-200"><a target="_blank" href="{{ $paymentinfo->prove_img }}"><img src="{{ $paymentinfo->prove_img }}" alt="" srcset=""></a></span>
 
                                 </div>
-                                </div>
-
 
                             </td>
                             <td class="md:p-3 hidden md:block">
