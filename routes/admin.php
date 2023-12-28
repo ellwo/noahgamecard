8fdea282779c6749ebb66d49473c0e8ddac946f3
mrzoom <mrzoom@zoom.zoom.com>
2023-12-28 21:54:26 +0300

    With Uploade Prove

--- routes/admin.php
+++ routes/admin.php
@@ -24,8 +24,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/uploade_prov_image', [UploadeController::class, 'uploade_prov']);
-
 Route::prefix('admin')->middleware(['auth'])->group(function () {
 
     Route::resource('ad',AdController::class)->name('index','ad');
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\UploadeController;
 use App\Http\Controllers\PaymentmethodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::post('/uploade_prov_image', [UploadeController::class, 'uploade_prov']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
