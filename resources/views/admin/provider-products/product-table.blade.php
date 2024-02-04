8b3753d6d8179202e2803ff31f898651895f6cd5
ellwo <ellw0pc@gmail.com>
2024-02-04 22:55:16 +0300

    fix Some in Payemnt info

--- resources/views/admin/provider-products/product-table.blade.php
+++ resources/views/admin/provider-products/product-table.blade.php
@@ -4,15 +4,15 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
 
-    <div class=" overflow-x-auto bg-transparent w-full flex items-center justify-center  font-sans ">
+    <div class="flex items-center justify-center w-full overflow-x-auto font-sans bg-transparent ">
         <div class="w-full lg:w-5/6">
-            <div class="rounded-t bg-white dark:bg-darker mb-4 mt-4 px-4 py-3 border-0">
+            <div class="px-4 py-3 mt-4 mb-4 bg-white border-0 rounded-t dark:bg-darker">
                 <div dir="rtl" class="flex flex-wrap items-center">
-                    <div class="relative w-full sm:px-4 max-w-full flex-grow sm:flex-1">
-                        <h3 class="font-semibold relative mb-4 flex text-base text-blueGray-700">
+                    <div class="relative flex-grow w-full max-w-full sm:px-4 sm:flex-1">
+                        <h3 class="relative flex mb-4 text-base font-semibold text-blueGray-700">
                             {{ __('منتجات المزودين') }}
-                            <x-bi-cart-fill class="w-10 h-10 text-yellow-400 flex" />
-                            <span class="rounded-full p-1 bg-indigo-500 ">+</span>
+                            <x-bi-cart-fill class="flex w-10 h-10 text-yellow-400" />
+                            <span class="p-1 bg-indigo-500 rounded-full ">+</span>
 
                         </h3>
                         <div class="relative md:w-full">
@@ -29,7 +29,7 @@
                                 </svg>
                             </div>
                         </div>
-                        <select  wire:model="deptid" class=" form-select flex-1 dark:bg-darker rounded-full m-2"
+                        <select  wire:model="deptid" class="flex-1 m-2 rounded-full  form-select dark:bg-darker"
                             id="color1">
                             <option  value="all"> القسم /الجميع
                             </option>
@@ -38,7 +38,7 @@
                                     {{ $dept->name }}</option>
                             @endforeach
                         </select>
-                        <select  wire:model="client" class=" form-select flex-1 dark:bg-darker rounded-full m-2"
+                        <select  wire:model="client" class="flex-1 m-2 rounded-full  form-select dark:bg-darker"
                             id="color1">
                             <option  value="all"> المزود /الجميع
                             </option>
@@ -47,16 +47,16 @@
                                    {{ $dept->name }}</option>
                             @endforeach
                         </select>
-                        <div class="w-full  mb-4 flex" dir="auto">
-                            <label class=" text-xs  dark:text-white dark:bg-darker text-black sm:px-4" for="bydate">ترتيب
+                        <div class="flex w-full mb-4" dir="auto">
+                            <label class="text-xs text-black  dark:text-white dark:bg-darker sm:px-4" for="bydate">ترتيب
                                 التاريخ</label>
-                            <select class=" rounded-md border text-sm flex-1 dark:bg-darker " id="bydate" wire:model="dateorder">
+                            <select class="flex-1 text-sm border rounded-md  dark:bg-darker" id="bydate" wire:model="dateorder">
                                 <option value="no">بلا</option>
                                 <option value="ASC"> تصاعدي</option>
                                 <option value="DESC">تنازلي</option>
                             </select>
-                            <label class=" text-xs dark:text-white sm:px-4 dark:bg-darker text-black" for="byprice">ترتيب السعر</label>
-                            <select class=" rounded-md border text-sm flex-1 dark:bg-darker " wire:model="priceorder" id="byprice">
+                            <label class="text-xs text-black  dark:text-white sm:px-4 dark:bg-darker" for="byprice">ترتيب السعر</label>
+                            <select class="flex-1 text-sm border rounded-md  dark:bg-darker" wire:model="priceorder" id="byprice">
                                 <option value="no">بلا</option>
                                 <option value="ASC"> تصاعدي</option>
                                 <option value="DESC">تنازلي</option>
@@ -64,10 +64,10 @@
                         </div>
 
                     </div>
-                    <div class="relative w-full px-4 max-w-full  flex-1 text-right">
+                    <div class="relative flex-1 w-full max-w-full px-4 text-right">
 
 
-                        <a href="{{route("provider_products.create")}}" class="cursor-pointer text-white w-1/2 mx-auto bg-blue-900 btn flex rounded-md  p-2 mb-2  ">
+                        <a href="{{route("provider_products.create")}}" class="flex w-1/2 p-2 mx-auto mb-2 text-white bg-blue-900 rounded-md cursor-pointer btn ">
                             اضافة منتج
                             <x-bi-cart class="w-6 h-6 text-yellow-400" />
                         </a>
@@ -82,7 +82,7 @@
 
                 </div>
 
-                <div class="flex mt-4 dark:text-white flex-col ">
+                <div class="flex flex-col mt-4 dark:text-white ">
 
 
                     {{ $products->links() }}
@@ -90,18 +90,18 @@
 
 
                 <div
-                    class="bg-white overflow-x-auto relative dark:bg-darker dark:text-white w-full shadow-md rounded my-6">
+                    class="relative w-full my-6 overflow-x-auto bg-white rounded shadow-md dark:bg-darker dark:text-white">
 
-                    <div class="w-full top-0 bottom-0 z-30 bg-white bg-opacity-50 absolute" wire:loading>
-                        <div class="w-full h-4 bg-blue-900 mt-16 rounded animate-pulse top-10 bottom-0 my-auto"></div>
+                    <div class="absolute top-0 bottom-0 z-30 w-full bg-white bg-opacity-50" wire:loading>
+                        <div class="bottom-0 w-full h-4 my-auto mt-16 bg-blue-900 rounded animate-pulse top-10"></div>
                     </div>
 
 
 
 
 
-                    <h4 class="text-right flex my-2" dir="rtl">عدد العناصر
-                        <select wire:model='paginate_num'  class="bg-white  bg-white rounded-md text-dark text-xs dark:text-white dark:bg-dark border-0">
+                    <h4 class="flex my-2 text-right" dir="rtl">عدد العناصر
+                        <select wire:model='paginate_num'  class="text-xs bg-white border-0 rounded-md text-dark dark:text-white dark:bg-dark">
                             <option value="10">10</option>
                             <option value="10">15</option>
                             <option value="20">20</option>
@@ -117,49 +117,49 @@
                         </h4>
 
 
-                    <table dir="rtl" class="min-w-max w-full table-auto overflow-x-auto">
+                    <table dir="rtl" class="w-full overflow-x-auto table-auto min-w-max">
                         <thead>
-                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm ">
+                            <tr class="text-sm text-gray-600 uppercase bg-gray-200 ">
 
-                                <th class="py-3 px-2  text-right">المنتج
+                                <th class="px-2 py-3 text-right">المنتج
                                 </th>
-                                <th class="py-3 px-2  text-right">المزود
-
-                                </th>
-                                <th class="py-3 px-2  text-right"> الحالة</th>
-                                <th class="py-3  text-right">السعر
+                                <th class="px-2 py-3 text-right">المزود
 
                                 </th>
+                                <th class="px-2 py-3 text-right"> الحالة</th>
+                                <th class="py-3 text-right">السعر
 
-                                <th class="py-3  text-center">Actions</th>
+                                </th>
+
+                                <th class="py-3 text-center">Actions</th>
                             </tr>
                         </thead>
-                        <tbody class="text-gray-600 dark:text-white text-sm font-light">
+                        <tbody class="text-sm font-light text-gray-600 dark:text-white">
 
 
                             @foreach ($products as $product)
                                 <tr
-                                    class="border-b-2 border-dark relative border-gray-200 hover:bg-gray-100 dark:hover:bg-dark dark:hover:text-white">
-                                    <td class="py-3 px-2 mb-4 text-right">
+                                    class="relative border-b-2 border-gray-200 border-dark hover:bg-gray-100 dark:hover:bg-dark dark:hover:text-white">
+                                    <td class="px-2 py-3 mb-4 text-right">
                                         <div class="flex items-center">
 
                                             <span dir="auto" class="font-bold ">{{ $product->name }}
                                                 <span
-                                                    class="block  text-blue-700 dark:text-white  font-light text-xs ">
+                                                    class="block text-xs font-light text-blue-700 dark:text-white ">
 
-                                                        المنتج الاساسي / {{ $product->product->?name??"" }}
+                                                        المنتج الاساسي / {{ $product->product?->name??"" }}
 
                                                 </span>
                                                 <span
-                                                class="block  text-blue-700 dark:text-white  font-light text-xs ">
+                                                class="block text-xs font-light text-blue-700 dark:text-white ">
 
-                                                    القسم/ {{ $product->product->?department->name??"" }}
+                                                    القسم/ {{ $product->product?->department->name??"" }}
                                             </span>
 
                                             </span>
                                         </div>
                                         <div>
-                                            <span class=" mb-6 mt-4 text-green-600 font-bold py-1 px-1  text-xs">
+                                            <span class="px-1 py-1 mt-4 mb-6 text-xs font-bold text-green-600 ">
                                                 {{ date('d/M/Y', strtotime($product->updated_at)) }}
 
                                             </span>
@@ -177,41 +177,41 @@
 
                                         @if ($product->active)
                                         <span
-                                        class="bg-success-dark mb-2  text-white font-bold p-2 px-3 rounded-full ">
+                                        class="p-2 px-3 mb-2 font-bold text-white rounded-full bg-success-dark ">
                                         نشط
                                     </span>
                                         @else
                                         <span
-                                        class="bg-danger mb-2  text-white font-bold p-2 px-3 rounded-full ">
+                                        class="p-2 px-3 mb-2 font-bold text-white rounded-full bg-danger ">
                                     غير نشط
                                     </span>
 
                                         @endif
                                     </td>
-                                    <td class="py-3 text-md font-bold text-center">
+                                    <td class="py-3 font-bold text-center text-md">
                                         <div class="flex flex-col w-1/2">
                                             <span>   سعر المنتج الاساسي
                                             </span>
                                             <span
-                                            class="bg-info mb-2  text-white font-bold p-2 px-3 rounded-full ">
+                                            class="p-2 px-3 mb-2 font-bold text-white rounded-full bg-info ">
 
                                             {{ $product->product->price."  /$" }}
                                         </span>
                                         <span>سعر المزود</span>
 
                                         <span
-                                        class="bg-success-dark mb-2 text-white font-bold p-2 px-3 rounded-full ">
+                                        class="p-2 px-3 mb-2 font-bold text-white rounded-full bg-success-dark ">
                                         {{ $product->price."  /$" }}
                                     </span>
                                         </div>
                                     </td>
 
-                                    <td class="py-3   text-right">
-                                        <div class="flex item-center justify-center">
+                                    <td class="py-3 text-right">
+                                        <div class="flex justify-center item-center">
 
                                             <a href="{{ route('provider_products.edit', $product) }}"
                                             {{-- wire:click="setDeleteproid({{ $product->id }})" --}}
-                                                class="w-4 flex mr-2 mt-2 cursor-pointer transform hover:text-red-800 hover:scale-110">
+                                                class="flex w-4 mt-2 mr-2 transform cursor-pointer hover:text-red-800 hover:scale-110">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -227,34 +227,34 @@
             <div x-transition:enter="transition duration-500 ease-in-out" x-transition:enter-start="opacity-0"
             x-transition:enter-end="opacity-100" x-transition:leave="transition duration-500 ease-in-out"
             x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-            class="fixed inset-0 bg-darker opacity-10 z-30" @click='dpm{{ $deleteproid }}=!dpm{{ $deleteproid }}'
+            class="fixed inset-0 z-30 bg-darker opacity-10" @click='dpm{{ $deleteproid }}=!dpm{{ $deleteproid }}'
             wire:click="setDeleteproid('no')"
             x-show="dpm{{ $deleteproid }}">
 
         </div>
-                                                <div class="dialog-content p-4 w-1/3  fixed z-50 bg-white top-1/3 left-1/2 rounded-md">
+                                                <div class="fixed z-50 w-1/3 p-4 bg-white rounded-md dialog-content top-1/3 left-1/2">
                                                     <div class="dialog-header dark:text-black">
                                                     تعديل حالة المنتج
                                                     </div>
                                                     <div class="dialog-body lg:flex" dir="auto">
-                                                        <h1 class="text-sm text-red-800 font-bold p-4 rounded-lg ">
+                                                        <h1 class="p-4 text-sm font-bold text-red-800 rounded-lg ">
 
                                                         </h1>
                                                     </div>
-                                                    <div class="dialog-footer flex justify-between  mx-auto">
+                                                    <div class="flex justify-between mx-auto dialog-footer">
                                                         <x-button
 
                                                         wire:click="active({{$deleteproid}})"
                                                         @click="dpm{{ $deleteproid }}=!dpm{{ $deleteproid }}"
                                                         variant='success' type="button"
-                                                        class=" text-white rounded-box p-2 "
+                                                        class="p-2 text-white  rounded-box"
                                                         >تنشيط </x-button>
 
                                                             <button type="button"
                                                             @click="dpm{{ $deleteproid }}=!dpm{{ $deleteproid }}"
 
                                                             wire:click="deactive({{ $deleteproid }})"
-                                                            class="bg-red-700 text-white rounded-box p-2">الغاء التنشيط </button>
+                                                            class="p-2 text-white bg-red-700 rounded-box">الغاء التنشيط </button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -280,7 +280,7 @@
 
 
     @if (session()->get('statt') == 'ok')
-        <div class="fixed p-4  z-50  bg-green-500 rounded-md text-white top-10 w-1/4 mx-auto right-5 toast" role="alert"
+        <div class="fixed z-50 w-1/4 p-4 mx-auto text-white bg-green-500 rounded-md top-10 right-5 toast" role="alert"
             x-on:toast1.window="open = !open" x-data="{ open: true }" x-show.transition="open" x-cloak>
             <div class="flex items-center justify-between mb-1">
                 <span class="font-bold text-blue-600">
